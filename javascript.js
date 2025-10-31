763446d325d0216afa4e5699c66931d18d60989d
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-10-31 10:11:59 -0600

    Write the logic to get the computer choice - add the getComputerChoice func

--- javascript.js
+++ javascript.js
@@ -1,16 +1,17 @@
-/*
- * Write a function that randomly generates the computers choice
- * Use Math.random func
- * named: `getComputerChoice`
- * Create a function to get the human choice
- * named: `getHumanChoice`
- * use [prompt](https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt)  method to accept the input
- * Create two variables to track users scores
- * `humanScore` and `computerScore`
- * both start at 0
- * Write a function to play a single round
- * named: `playRound`
- * has two params: humanChoice and computerChoice.
- * Increment the score for whoever won the round
- * Make a full game last 5 rounds
- */
+function getComputerChoice() {
+  const randomRoll = Math.floor(Math.random() * 100);
+
+  switch (true) {
+    case randomRoll <= 33:
+      return "paper";
+    case randomRoll <= 66:
+      return "scissors";
+    case randomRoll <= 100:
+      return "rock";
+    default:
+      return "paper";
+  }
+}
+
+const computerRoll = getComputerChoice();
+// console.log(`### roll: ${computerRoll}`);
