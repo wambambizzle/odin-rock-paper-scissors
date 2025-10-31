64eac9ffac447a26e4d37f22242ce04101132cd7
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-10-31 09:38:17 -0600

    Add a plan.md for pseudocode

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Suckaaa");
+/*
+ * Write a function that randomly generates the computers choice
+ * Use Math.random func
+ * named: `getComputerChoice`
+ * Create a function to get the human choice
+ * named: `getHumanChoice`
+ * use [prompt](https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt)  method to accept the input
+ * Create two variables to track users scores
+ * `humanScore` and `computerScore`
+ * both start at 0
+ * Write a function to play a single round
+ * named: `playRound`
+ * has two params: humanChoice and computerChoice.
+ * Increment the score for whoever won the round
+ * Make a full game last 5 rounds
+ */
