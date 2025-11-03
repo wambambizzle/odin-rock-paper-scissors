818a3325eb934d33dfffb9044442ec1f7685efa8
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-03 11:38:43 -0500

    Add the playRound logic - add func to play a round - takes human and computer choices as params - increments the score

--- javascript.js
+++ javascript.js
@@ -24,4 +24,48 @@ const computerRoll = getComputerChoice();
 const humanRoll = getHumanChoice();
 
 // console.log(`### comp roll: ${computerRoll}`);
-// console.log(`### human roll: ${humanRoll}`);
+// console.log(`### human choice: ${humanRoll}`);
+
+function playRound(humanChoice, computerChoice) {
+  let adjustedChoice = humanChoice.toLowerCase();
+
+  // console.log(`### inner comp roll: ${computerChoice}`);
+  // console.log(`### inner human roll: ${adjustedChoice}`);
+
+  if (adjustedChoice == computerChoice) {
+    return console.log("It's a tie!");
+  }
+
+  if (adjustedChoice == "rock" && computerChoice == "scissors") {
+    humanScore += 1;
+    return console.log("You win! Rock beats Scissors");
+  }
+
+  if (adjustedChoice == "rock" && computerChoice == "paper") {
+    computerScore += 1;
+    return console.log("You lose! Paper beats Rock");
+  }
+
+  if (adjustedChoice == "paper" && computerChoice == "rock") {
+    humanScore += 1;
+    return console.log("You win! Paper beats Rock");
+  }
+
+  if (adjustedChoice == "paper" && computerChoice == "scissors") {
+    computerScore += 1;
+    return console.log("You lose! Scissors beats Paper");
+  }
+
+  if (adjustedChoice == "scissors" && computerChoice == "paper") {
+    humanScore += 1;
+    return console.log("You win! Scissors beats Paper");
+  }
+
+  if (adjustedChoice == "scissors" && computerChoice == "rock") {
+    computerScore += 1;
+    return console.log("You lose! Rock beats Scissors");
+  }
+}
+
+playRound(humanRoll, computerRoll);
+// console.log(`$$$$ humanScore: ${humanScore}, computerScore: ${computerScore}`);
