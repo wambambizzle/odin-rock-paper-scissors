88d5a9216f5201f9858ec5103f702ff8967f4580
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-07 11:30:29 -0500

    Sync buttons to event handlers to play a round

--- javascript.js
+++ javascript.js
@@ -19,6 +19,7 @@ function getHumanChoice() {
 
 function playRound(humanChoice, computerChoice) {
   let adjustedChoice = humanChoice.toLowerCase();
+  console.log(`Your choice: ${adjustedChoice}`);
 
   if (adjustedChoice == computerChoice) {
     console.log("It's a tie!");
@@ -56,3 +57,15 @@ function playRound(humanChoice, computerChoice) {
     return false;
   }
 }
+
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    console.log(`Comp choice: ${computerChoice}`);
+    playRound(button.id, computerChoice);
+  });
+});
